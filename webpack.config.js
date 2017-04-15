2c74c59cb38858f162516fec5793f84d9ddcff43
Renato Massaro <renato@hackerexperience.com>
2017-04-15 05:12:09 -0300

    Better directory layout for css, js resources (our own) and vendors (3rd-parties)

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 var autoprefixer      = require( 'autoprefixer' );
 var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 var CopyWebpackPlugin = require( 'copy-webpack-plugin' );
-var entryPath         = path.join( __dirname, 'src/static/index.js' );
+var entryPath         = path.join( __dirname, 'src/static/js/index.js' );
 var outputPath        = path.join( __dirname, 'dist' );
 
 console.log( 'WEBPACK GO!');
