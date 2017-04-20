6e366dcb052011a269d4daa25beb7b73e1fe034b
PedroHLC <root@pedrohlc.com>
2017-04-20 15:43:07 -0300

    LogViewer: Icons placeholder

--- src/Apps/LogViewer/Style.elm
+++ src/Apps/LogViewer/Style.elm
@@ -33,9 +33,71 @@ type Classes
     | IcoHome
 
 
+ico : Mixin
+ico =
+    mixin
+        [ before
+            [ Icon.fontFamily
+            , textAlign center
+            ]
+        ]
+
+
 css : Stylesheet
 css =
     (stylesheet << namespace "logvw")
         [ class HeaderBar
             []
+        , class BtnUser
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class BtnEdit
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class BtnView
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class BtnOrder
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class BtnLock
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class BtnDelete
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class BtnUnlock
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class BtnApply
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class BtnCancel
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class CasedBtnExpand
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class IcoCrosshair
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class IcoUser
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
+        , class IcoHome
+            [ ico
+            , before [ Icon.fileGeneric ]
+            ]
         ]
