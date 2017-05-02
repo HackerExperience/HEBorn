009cabab6fb1d780424babac3ecbfd3065e9e978
PedroHLC <root@pedrohlc.com>
2017-05-02 10:30:42 -0300

    LogViewer: #62 applied here

--- src/Apps/LogViewer/View.elm
+++ src/Apps/LogViewer/View.elm
@@ -49,15 +49,142 @@ root =
 
 type LogEventMsg
     = LogIn NetAddr SysUser
+    | Connetion NetAddr NetAddr NetAddr
+    | ExternalAcess SysUser SysUser
+
+
+renderAddr : NetAddr -> List (Html Msg)
+renderAddr addr =
+    if (addr == localhost) then
+        [ span [ class [ IcoHome, ColorLocal ] ] []
+        , text " "
+        , span [ class [ IdLocal, ColorLocal ] ] [ text localhost ]
+        ]
+    else
+        [ span [ class [ IcoCrosshair, ColorRemote ] ] []
+        , text " "
+        , span [ class [ IdMe, ColorRemote ] ] [ text addr ]
+        ]
+
+
+renderUser : SysUser -> List (Html Msg)
+renderUser user =
+    if (user == root) then
+        [ span [ class [ IcoUser, ColorRoot ] ] []
+        , text " "
+        , span [ class [ IdRoot, ColorRoot ] ] [ text user ]
+        ]
+    else
+        [ span [ class [ IcoUser ] ] []
+        , text " "
+        , span [] [ text user ]
+        ]
+
+
+renderButtons : List Classes -> List (Html Msg)
+renderButtons btns =
+    case List.tail (List.concat (List.map (\d -> [ text " ", span [ class [ d ] ] [] ]) btns)) of
+        Nothing ->
+            []
+
+        Just x ->
+            x
+
+
+renderMsg : LogEventMsg -> Html Msg
+renderMsg msg =
+    (case msg of
+        LogIn addr user ->
+            div [ class [ EData ] ]
+                (renderAddr addr
+                    ++ [ span [] [ text " logged in as " ] ]
+                    ++ renderUser user
+                )
+
+        Connetion actor src dest ->
+            div [ class [ EData ] ]
+                (renderAddr actor
+                    ++ [ span [] [ text " bounced connection from " ]
+                       , span [ class [ IcoCrosshair, ColorRemote ] ] []
+                       , text " "
+                       , span [ class [ IdMe, ColorRemote ] ] [ text src ]
+                       , span [] [ text " to " ]
+                       , span [ class [ IcoDangerous, ColorDangerous ] ] []
+                       , text " "
+                       , span [ class [ IdOther, ColorDangerous ] ] [ text dest ]
+                       ]
+                )
+
+        ExternalAcess whom aswho ->
+            div [ class [ EData, BoxifyMe ] ]
+                (renderUser whom
+                    ++ [ span [] [ text " logged in as " ] ]
+                    ++ renderUser aswho
+                )
+    )
+
+
+renderTopActions : LogEventMsg -> Html Msg
+renderTopActions msg =
+    div [ class [ ETActMini ] ]
+        (case msg of
+            LogIn addr user ->
+                renderButtons [ BtnEdit ]
+
+            Connetion actor src dest ->
+                renderButtons [ BtnUser, BtnEdit ]
+
+            ExternalAcess whom aswho ->
+                []
+        )
+
+
+renderBottomActions : LogEventMsg -> Html Msg
+renderBottomActions msg =
+    div [ class [ EAct ] ]
+        (case msg of
+            LogIn addr user ->
+                []
+
+            Connetion actor src dest ->
+                renderButtons [ IcoUser, BtnView, BtnEdit, BtnDelete ]
+
+            ExternalAcess whom aswho ->
+                renderButtons [ BtnApply, BtnCancel ]
+        )
+
+
+attachVisibility status =
+    attribute "data-expanded"
+        (if (status) then
+            "1"
+         else
+            "0"
+        )
+
+
+renderEntry : String -> Bool -> LogEventMsg -> Html Msg
+renderEntry timestamp fullvisible msg =
+    div [ class [ Entry ] ]
+        [ div [ class [ ETop ] ]
+            [ div [] [ text timestamp ]
+            , div [ elasticClass ] []
+            , renderTopActions msg
+            ]
+        , renderMsg msg
+        , div [ class [ EBottom ] ]
+            [ renderBottomActions msg
+            , div
+                [ class [ CasedBtnExpand, EToggler ]
+                , attachVisibility fullvisible
+                ]
+                []
+            ]
+        ]
 
 
 
 -- END OF THAT
-
-
-renderEntry : Int -> Bool -> LogEventMsg -> Html Msg
-renderEntry timestamp fullvisible msg =
-    div [] []
 
 
 view : Model -> InstanceID -> GameModel -> Html Msg
@@ -82,64 +209,8 @@ view model id game =
                         ]
                     ]
                 ]
-            , div [ class [ Entry ] ]
-                [ div [ class [ ETop ] ]
-                    [ div [] [ text "15/03/2016 - 20:24:33.105" ]
-                    , div [ elasticClass ] []
-                    , div [ class [ ETActMini ] ]
-                        [ span [ class [ BtnEdit ] ] []
-                        ]
-                    ]
-                , div [ class [ EData ] ]
-                    [ span [ class [ IcoCrosshair, ColorRemote ] ] []
-                    , text " "
-                    , span [ class [ IdMe, ColorRemote ] ] [ text "174.57.204.104" ]
-                    , span [] [ text " logged in as " ]
-                    , span [ class [ IcoUser, ColorRoot ] ] []
-                    , text " "
-                    , span [ class [ IdRoot, ColorRoot ] ] [ text root ]
-                    ]
-                , div [ class [ EBottom ] ]
-                    [ div [ elasticClass ] []
-                    , div [ class [ CasedBtnExpand, EToggler ] ] []
-                    ]
-                ]
-            , div [ class [ Entry ] ]
-                [ div [ class [ ETop ] ]
-                    [ div [] [ text "15/03/2016 - 20:24:33.105" ]
-                    , div [ elasticClass ] []
-                    , div []
-                        [ span [ class [ BtnUser ] ] []
-                        , text " "
-                        , span [ class [ BtnEdit ] ] []
-                        ]
-                    ]
-                , div [ class [ EData ] ]
-                    [ span [ class [ IcoHome, ColorLocal ] ] []
-                    , text " "
-                    , span [ class [ IdLocal, ColorLocal ] ] [ text localhost ]
-                    , span [] [ text " bounced connection from " ]
-                    , span [ class [ IcoCrosshair, ColorRemote ] ] []
-                    , text " "
-                    , span [ class [ IdMe, ColorRemote ] ] [ text "174.57.204.104" ]
-                    , span [] [ text " to " ]
-                    , span [ class [ IcoDangerous, ColorDangerous ] ] []
-                    , text " "
-                    , span [ class [ IdOther, ColorDangerous ] ] [ text "209.43.107.189" ]
-                    ]
-                , div [ class [ EBottom ] ]
-                    [ div [ class [ EAct ] ]
-                        [ span [ class [ IcoUser ] ] []
-                        , text " "
-                        , span [ class [ BtnView ] ] []
-                        , text " "
-                        , span [ class [ BtnEdit ] ] []
-                        , text " "
-                        , span [ class [ BtnDelete ] ] []
-                        ]
-                    , div [ class [ CasedBtnExpand, EToggler ] ] []
-                    ]
-                ]
+            , renderEntry "15/03/2016 - 20:24:33.105" True (LogIn "174.57.204.104" root)
+            , renderEntry "15/03/2016 - 20:24:33.105" True (Connetion localhost "174.57.204.104" "209.43.107.189")
             , div [ class [ Entry ] ]
                 [ div [ class [ ETop ] ]
                     [ div [ elasticClass ] []
@@ -167,21 +238,5 @@ view model id game =
                         ]
                     ]
                 ]
-            , div [ class [ Entry ] ]
-                [ div [ class [ ETop ] ] [ text "15/03/2016 - 20:24:33.105" ]
-                , br [] []
-                , div [ class [ EData, BoxifyMe ] ]
-                    [ span [] [ text "NOTME" ]
-                    , span [] [ text " logged in as " ]
-                    , span [ class [ IcoUser ] ] []
-                    , span [] [ text root ]
-                    ]
-                , div [ class [ EBottom ] ]
-                    [ div [ class [ EAct ] ]
-                        [ span [ class [ BtnApply ] ] []
-                        , text " "
-                        , span [ class [ BtnCancel ] ] []
-                        ]
-                    ]
-                ]
+            , renderEntry "15/03/2016 - 20:24:33.105" True (ExternalAcess "NOTME" root)
             ]
