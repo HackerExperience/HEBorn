1f29a4820b15388c38bd0acd249a3cdd268157f1
PedroHLC <root@pedrohlc.com>
2017-05-30 10:17:25 -0300

    LogViewer: Fix titlebar icon

--- src/OS/WindowManager/Style.elm
+++ src/OS/WindowManager/Style.elm
@@ -87,7 +87,7 @@ css =
         , attrSelector "wmHeaderTitle"
             "data-icon"
             "="
-            "logviewer"
+            "logvw"
             [ before
                 [ Icon.logvw ]
             ]
