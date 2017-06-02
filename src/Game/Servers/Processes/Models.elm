b65545d06c84bb555c6950f70a76c72ad95c8031
PedroHLC <root@pedrohlc.com>
2017-06-02 17:57:29 -0300

    Game.Processes: Recreated following @mememori specs

--- src/Game/Servers/Processes/Models.elm
+++ src/Game/Servers/Processes/Models.elm
@@ -2,82 +2,19 @@ module Game.Servers.Processes.Models exposing (..)
 
 import Dict
 import Utils
-import Time exposing (Time)
-import Game.Servers.Filesystem.Models exposing (FileID)
-import Game.Shared exposing (ID)
+import Game.Servers.Processes.Types.Shared exposing (..)
+import Game.Servers.Processes.Types.Local as Local exposing (ProcessProp, ProcessState(..))
+import Game.Servers.Processes.Types.Remote as Remote exposing (ProcessProp)
 
 
-type alias ProcessID =
-    ID
-
-
-type alias ServerID =
-    ID
-
-
-type alias ConnectionID =
-    ID
-
-
-type ProcessType
-    = Cracker
-    | Decryptor
-    | Encryptor
-    | FileDownload
-    | LogDeleter
-
-
-type alias CompletionDate =
-    Time
-
-
-type ProcessState
-    = StateRunning CompletionDate
-    | StateStandby
-    | StatePaused
-    | StateComplete
-
-
-type alias ProcessPriority =
-    Int
-
-
-type alias Progress =
-    Float
-
-
-type alias GatewayID =
-    ServerID
-
-
-type alias TargetServerID =
-    ServerID
-
-
-{-| FIXME: export NetworkID from Game.Network.Models
--}
-type alias NetworkID =
-    String
+type ProcessProp
+    = LocalProcess Local.ProcessProp
+    | RemoteProcess Remote.ProcessProp
 
 
 type alias Process =
     { id : ProcessID
-    , processType : ProcessType
-    , priority : ProcessPriority
-    , state : ProcessState
-    , progress : Progress
-    , fileID : Maybe FileID
-    , version : Maybe Float
-    , gatewayID : GatewayID
-    , targetServerID : TargetServerID
-    , networkID : NetworkID
-    , connectionID : ConnectionID
-
-    -- REVISE: USAGE WASN'T IMPLEMENTED YET
-    , cpuUsage : Float
-    , memusage : Float
-    , downloadUsage : Float
-    , uploadUsage : Float
+    , prop : ProcessProp
     }
 
 
@@ -89,13 +26,7 @@ initialProcesses : Processes
 initialProcesses =
     Dict.fromList
         -- DUMMY VALUE FOR PLAYING
-        [ ( "dummy0000"
-          , (Process "dummy0000" Cracker 1 (StateRunning 0) 0.5 (Just "dummym0") (Just 1.1) "me" "you" "san" "francisco" 1900000000 786000000 0 0)
-          )
-        , ( "dummy0001"
-          , (Process "dummy0001" Decryptor 1 (StateRunning 0) 0.7 (Just "dummym1") (Just 2.0) "you" "me" "new" "york" 1900000000 786000000 512000 256000)
-          )
-        ]
+        []
 
 
 {-| REVIEW: this doesn't look that useful
@@ -125,43 +56,33 @@ removeProcess processes process =
     Dict.remove process.id processes
 
 
+doLocalProcess : (Local.ProcessProp -> Local.ProcessProp) -> Processes -> Process -> Processes
+doLocalProcess job processes process =
+    case process.prop of
+        LocalProcess prop ->
+            let
+                localProp_ =
+                    job prop
+
+                prop_ =
+                    LocalProcess localProp_
+            in
+                Utils.safeUpdateDict processes process.id { process | prop = prop_ }
+
+        _ ->
+            processes
+
+
 pauseProcess : Processes -> Process -> Processes
-pauseProcess processes process =
-    case process.state of
-        StatePaused ->
-            processes
-
-        _ ->
-            let
-                process_ =
-                    { process | state = StatePaused }
-            in
-                Utils.safeUpdateDict processes process_.id process_
+pauseProcess =
+    doLocalProcess (\process -> { process | state = StatePaused })
 
 
-resumeProcess : CompletionDate -> Processes -> Process -> Processes
-resumeProcess completionDate processes process =
-    case process.state of
-        StateRunning _ ->
-            processes
-
-        _ ->
-            let
-                process_ =
-                    { process | state = StateRunning completionDate }
-            in
-                Utils.safeUpdateDict processes process_.id process_
+resumeProcess : Processes -> Process -> Processes
+resumeProcess =
+    doLocalProcess (\process -> { process | state = StateRunning })
 
 
 completeProcess : Processes -> Process -> Processes
-completeProcess processes process =
-    case process.state of
-        StateComplete ->
-            processes
-
-        _ ->
-            let
-                process_ =
-                    { process | state = StateComplete }
-            in
-                Utils.safeUpdateDict processes process_.id process_
+completeProcess =
+    doLocalProcess (\process -> { process | state = StateComplete })
