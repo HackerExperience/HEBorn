17150a8345421f28a11e9c476dda8da098015255
PedroHLC <root@pedrohlc.com>
2017-06-06 10:19:03 -0300

    TaskManager: Alter Game.Servers.Processes; Completes tasks when done.

--- src/Apps/TaskManager/Update.elm
+++ src/Apps/TaskManager/Update.elm
@@ -1,20 +1,54 @@
 module Apps.TaskManager.Update exposing (update)
 
-import Core.Messages exposing (CoreMsg)
+import Dict
+import Utils exposing (andThenWithDefault)
+import Core.Messages exposing (CoreMsg(MsgGame))
 import Game.Models exposing (GameModel)
-import Game.Servers.Processes.Models exposing (initialProcesses)
+import Game.Messages exposing (GameMsg(MsgServers))
+import Game.Servers.Models exposing (ServerID)
+import Game.Servers.Messages exposing (ServerMsg(MsgProcess))
+import Game.Servers.Processes.Types.Local exposing (ProcessState(StateRunning))
+import Game.Servers.Processes.Models exposing (ProcessProp(LocalProcess))
+import Game.Servers.Processes.Messages as Processes exposing (Msg(..))
 import Apps.TaskManager.Models
     exposing
         ( Model
+        , onlyLocalTasks
         , updateTasks
         )
-import Apps.TaskManager.Messages exposing (Msg(..))
+import Apps.TaskManager.Messages as TaskManager exposing (Msg(..))
 import Apps.TaskManager.Menu.Messages as MsgMenu
 import Apps.TaskManager.Menu.Update
 import Apps.TaskManager.Menu.Actions exposing (actionHandler)
 
 
-update : Msg -> GameModel -> Model -> ( Model, Cmd Msg, List CoreMsg )
+msgProcesses : ServerID -> Processes.Msg -> CoreMsg
+msgProcesses serverID msg =
+    MsgProcess serverID msg
+        |> MsgServers
+        |> MsgGame
+
+
+processComplete tasks now =
+    List.filterMap
+        (\process ->
+            case process.prop of
+                LocalProcess prop ->
+                    if
+                        (prop.state == StateRunning)
+                            && (andThenWithDefault (\eta -> now > (Debug.log "ETA: " eta)) False prop.eta)
+                    then
+                        Just (msgProcesses "localhost" (Processes.Complete process.id))
+                    else
+                        Nothing
+
+                _ ->
+                    Nothing
+        )
+        (Dict.values tasks)
+
+
+update : TaskManager.Msg -> GameModel -> Model -> ( Model, Cmd TaskManager.Msg, List CoreMsg )
 update msg game ({ app } as model) =
     case msg of
         -- -- Context
@@ -32,7 +66,7 @@ update msg game ({ app } as model) =
                 ( { model | menu = menu_ }, cmd_, coreMsg )
 
         --- Every update
-        Tick _ ->
+        Tick now ->
             let
                 newApp =
                     updateTasks
@@ -40,5 +74,8 @@ update msg game ({ app } as model) =
                         --TODO: Recalculate limits
                         app.limits
                         app
+
+                completeMsgs =
+                    processComplete app.localTasks now
             in
-                ( { model | app = newApp }, Cmd.none, [] )
+                ( { model | app = newApp }, Cmd.none, completeMsgs )
