f0590e039ba149ab341081c305e1be9c8cda7fcf
kress95 <cferraz95@gmail.com>
2017-06-09 19:29:15 -0300

    New Event framework.

--- src/Game/Messages.elm
+++ src/Game/Messages.elm
@@ -4,6 +4,7 @@ import Game.Account.Messages
 import Game.Servers.Messages
 import Game.Network.Messages
 import Game.Meta.Messages
+import Events.Events as Events
 
 
 type GameMsg
@@ -11,4 +12,5 @@ type GameMsg
     | MsgServers Game.Servers.Messages.ServerMsg
     | MsgNetwork Game.Network.Messages.NetworkMsg
     | MsgMeta Game.Meta.Messages.MetaMsg
+    | Event Events.Response
     | NoOp
--- src/Game/Network/Messages.elm
+++ src/Game/Network/Messages.elm
@@ -1,5 +1,8 @@
 module Game.Network.Messages exposing (NetworkMsg(..))
 
+import Events.Events as Events
+
 
 type NetworkMsg
     = ToDo
+    | Event Events.Response
