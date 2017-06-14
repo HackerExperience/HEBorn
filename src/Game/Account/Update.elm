48e1ff8e3edb3d344f83f3c12fdc1f57101ba9a7
kress95 <cferraz95@gmail.com>
2017-06-14 17:53:48 -0300

    Enable 'server.index' request.

--- src/Game/Account/Update.elm
+++ src/Game/Account/Update.elm
@@ -2,10 +2,14 @@ module Game.Account.Update exposing (..)
 
 import Maybe
 import Core.Messages exposing (CoreMsg(MsgWebsocket))
+import Driver.Websocket.Reports as Websocket
+import Driver.Websocket.Channels as Websocket
+import Events.Events as Events
 import Game.Account.Messages exposing (..)
 import Game.Account.Models exposing (..)
 import Game.Account.Requests exposing (..)
 import Game.Account.Requests.Logout as Logout
+import Game.Account.Requests.ServerIndex as ServerIndex
 import Game.Models exposing (GameModel)
 
 
@@ -25,26 +29,12 @@ update msg model game =
         Request data ->
             response (receive data) model game
 
-        _ ->
-            ( model, Cmd.none, [] )
+        Event data ->
+            event data model game
 
 
 
 -- internals
-
-
-response :
-    Response
-    -> AccountModel
-    -> GameModel
-    -> ( AccountModel, Cmd msg, List CoreMsg )
-response response model game =
-    case response of
-        LogoutResponse Logout.OkResponse ->
-            ( model, Cmd.none, [] )
-
-        _ ->
-            ( model, Cmd.none, [] )
 
 
 login :
@@ -79,6 +69,38 @@ logout model game =
                     Logout.request token game.meta.config
             in
                 ( model_, cmd, [] )
+
+        _ ->
+            ( model, Cmd.none, [] )
+
+
+response :
+    Response
+    -> AccountModel
+    -> GameModel
+    -> ( AccountModel, Cmd msg, List CoreMsg )
+response response model game =
+    case response of
+        LogoutResponse Logout.OkResponse ->
+            ( model, Cmd.none, [] )
+
+        _ ->
+            ( model, Cmd.none, [] )
+
+
+event :
+    Events.Response
+    -> AccountModel
+    -> GameModel
+    -> ( AccountModel, Cmd AccountMsg, List CoreMsg )
+event ev model game =
+    case ev of
+        Events.Report (Websocket.Joined Websocket.AccountChannel) ->
+            let
+                cmd =
+                    ServerIndex.request (Maybe.withDefault "" model.id) game.meta.config
+            in
+                ( model, Cmd.none, [] )
 
         _ ->
             ( model, Cmd.none, [] )
