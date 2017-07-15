7ed1e4f60b4fae6b29d26c36012644844a0f103a
PedroHLC <root@pedrohlc.com>
2017-07-15 00:24:53 -0300

    Dock: add hover effect

--- src/OS/SessionManager/Dock/Style.elm
+++ src/OS/SessionManager/Dock/Style.elm
@@ -3,7 +3,7 @@ module OS.SessionManager.Dock.Style exposing (..)
 import Css exposing (..)
 import Css.Namespace exposing (namespace)
 import Css.Elements exposing (ul, li)
-import Css.Utils as Css exposing (pseudoContent, withAttribute)
+import Css.Utils as Css exposing (Easing(..), pseudoContent, withAttribute, transition)
 import Css.Common exposing (flexContainerHorz, globalShadow, emptyContent)
 import Css.Gradients as Gradients
 import Css.Icons as Icon
@@ -59,45 +59,6 @@ css =
             , zIndex (int 0)
             , cursor pointer
             ]
-        , class ItemIco
-            [ borderRadius (pct 100)
-            , padding (px 8)
-            , backgroundImage <|
-                linearGradient2
-                    toBottom
-                    (stop2 (hex "f3c5bd") (pct 0))
-                    (stop2 (hex "e86c57") (pct 50))
-                    [ (stop2 (hex "ff6600") (pct 51))
-                    , (stop <| hex "c72200")
-                    ]
-            , globalShadow
-            , before
-                [ Icon.fontFamily
-                , fontSize (px 24)
-                , minWidth (px 30)
-                , minHeight (px 30)
-                , textAlign center
-                , display inlineBlock
-                ]
-            , addIco "explorer" Icon.explorer
-            , addIco "logvw" Icon.logvw
-            , addIco "browser" Icon.browser
-            , addIco "taskmngr" Icon.taskMngr
-            , addIco "udb" Icon.dbAdmin
-            , addIco "connmngr" Icon.connMngr
-            , addIco "bouncemngr" Icon.bounceMngr
-            , addIco "moneymngr" Icon.finance
-            , addIco "hebamp" Icon.hebamp
-            , addGrad "explorer" Gradients.mangoPulp
-            , addGrad "logvw" Gradients.stellar
-            , addGrad "browser" Gradients.pinotNoir
-            , addGrad "taskmngr" Gradients.blurryBeach
-            , addGrad "udb" Gradients.calmDarya
-            , addGrad "connmngr" Gradients.influenza
-            , addGrad "bouncemngr" Gradients.bourbon
-            , addGrad "moneymngr" Gradients.army
-            , addGrad "hebamp" Gradients.veryBlue
-            ]
         , class Item
             [ margin3 (px 8) (px 4) (px 0)
             , zIndex (int 2)
@@ -111,9 +72,8 @@ css =
                 , marginTop (px -8)
                 , position absolute
                 , marginLeft (px 21)
+                , transition 0.25 "margin" EaseOut
                 ]
-            , hover
-                [ children [ class AppContext [ display block ] ] ]
             , withAttribute (Css.EQ "data-hasinst" "Y")
                 [ after
                     [ padding (px 2)
@@ -121,27 +81,82 @@ css =
                     , globalShadow
                     ]
                 ]
-            ]
-        , class AppContext
-            [ display none
-            , position absolute
-            , bottom (px 0)
-            , backgroundColor (rgba 0 0 0 0.5)
-            , marginBottom (px 50)
-            , width (px 180)
-            , maxHeight (vh 80)
-            , marginLeft (px ((-180 + 46) / 2)) -- (-DockAppContext.width + dockItem.width) / 2
-            , borderRadius (px 8)
-            , cursor pointer
-            , fontSize (px 12)
-            , withClass Visible
-                [ display block ]
             , children
-                [ ul
-                    [ padding (px 8)
-                    , listStyle none
+                [ class ItemIco
+                    [ borderRadius (pct 100)
+                    , padding (px 8)
+                    , backgroundImage <|
+                        linearGradient2
+                            toBottom
+                            (stop2 (hex "f3c5bd") (pct 0))
+                            (stop2 (hex "e86c57") (pct 50))
+                            [ (stop2 (hex "ff6600") (pct 51))
+                            , (stop <| hex "c72200")
+                            ]
+                    , globalShadow
+                    , before
+                        [ Icon.fontFamily
+                        , fontSize (px 24)
+                        , minWidth (px 30)
+                        , minHeight (px 30)
+                        , textAlign center
+                        , display inlineBlock
+                        ]
+                    , transition 0.25 "all" EaseOut
+                    , addIco "explorer" Icon.explorer
+                    , addIco "logvw" Icon.logvw
+                    , addIco "browser" Icon.browser
+                    , addIco "taskmngr" Icon.taskMngr
+                    , addIco "udb" Icon.dbAdmin
+                    , addIco "connmngr" Icon.connMngr
+                    , addIco "bouncemngr" Icon.bounceMngr
+                    , addIco "moneymngr" Icon.finance
+                    , addIco "hebamp" Icon.hebamp
+                    , addGrad "explorer" Gradients.mangoPulp
+                    , addGrad "logvw" Gradients.stellar
+                    , addGrad "browser" Gradients.pinotNoir
+                    , addGrad "taskmngr" Gradients.blurryBeach
+                    , addGrad "udb" Gradients.calmDarya
+                    , addGrad "connmngr" Gradients.influenza
+                    , addGrad "bouncemngr" Gradients.bourbon
+                    , addGrad "moneymngr" Gradients.army
+                    , addGrad "hebamp" Gradients.veryBlue
+                    ]
+                , class AppContext
+                    [ display none
+                    , position absolute
+                    , bottom (px 0)
+                    , backgroundColor (rgba 0 0 0 0.5)
+                    , marginBottom (px 50)
+                    , width (px 180)
+                    , maxHeight (vh 80)
+                    , marginLeft (px ((-180 + 46) / 2)) -- (-DockAppContext.width + dockItem.width) / 2
+                    , borderRadius (px 8)
+                    , cursor pointer
+                    , fontSize (px 12)
+                    , withClass Visible
+                        [ display block ]
                     , children
-                        [ li [ paddingLeft (px 8) ] ]
+                        [ ul
+                            [ padding (px 8)
+                            , listStyle none
+                            , children
+                                [ li [ paddingLeft (px 8) ] ]
+                            ]
+                        ]
+                    ]
+                ]
+            , hover
+                [ children
+                    [ class ItemIco
+                        [ transform <| scale 1.5
+                        , margin3 (px -12) (px 12) (px 0)
+                        ]
+                    , class AppContext [ display block ]
+                    ]
+                , after
+                    [ marginTop (px 0)
+                    , marginLeft (px 32)
                     ]
                 ]
             ]
