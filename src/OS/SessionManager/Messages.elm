3536c7bb1041103dc69ea041869903a432bf1dfc
kress95 <cferraz95@gmail.com>
2017-08-22 22:28:15 -0300

    Add a method to allow dispatching messages to Apps.

--- src/OS/SessionManager/Messages.elm
+++ src/OS/SessionManager/Messages.elm
@@ -2,8 +2,14 @@ module OS.SessionManager.Messages exposing (Msg(..))
 
 import OS.SessionManager.WindowManager.Messages as WindowManager
 import OS.SessionManager.Dock.Messages as Dock
+import Apps.Messages as Apps
+
+
+type alias ServerID =
+    String
 
 
 type Msg
     = WindowManagerMsg WindowManager.Msg
+    | AppMsg (Maybe ServerID) Apps.Msg
     | DockMsg Dock.Msg
