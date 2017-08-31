2c916ec1c3bbdfe295e835dc02778f996e92c480
kress95 <cferraz95@gmail.com>
2017-08-30 22:42:19 -0300

    Add process conclusion event.

--- src/Events/Servers/Processes.elm
+++ src/Events/Servers/Processes.elm
@@ -1,10 +1,24 @@
 module Events.Servers.Processes exposing (Event(..), handler)
 
+import Utils.Events exposing (Handler, notify)
+import Json.Decode
+    exposing
+        ( decodeValue
+        , oneOf
+        , map
+        , maybe
+        , lazy
+        , list
+        , string
+        , int
+        )
+import Json.Decode.Pipeline exposing (decode, required, optional)
 import Utils.Events exposing (Handler)
 
 
 type Event
     = Changed
+    | Conclusion String
 
 
 handler : String -> Handler Event
@@ -12,6 +26,9 @@ handler event json =
     case event of
         "changed" ->
             onChanged json
+
+        "conclusion" ->
+            onConclusion json
 
         _ ->
             Nothing
@@ -24,3 +41,15 @@ handler event json =
 onChanged : Handler Event
 onChanged json =
     Just Changed
+
+
+onConclusion : Handler Event
+onConclusion json =
+    let
+        decoder =
+            decode identity
+                |> required "process_id" string
+    in
+        decodeValue decoder json
+            |> Result.map Conclusion
+            |> notify
