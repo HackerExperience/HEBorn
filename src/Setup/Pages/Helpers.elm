0dc87ef75ee1e8e43811c03d53d0938e836ab89f
kress95 <cferraz95@gmail.com>
2017-10-25 15:19:48 -0200

    Fix revision concerns.

--- src/Setup/Pages/Helpers.elm
+++ src/Setup/Pages/Helpers.elm
@@ -7,4 +7,6 @@ import Setup.Resources exposing (..)
 
 withHeader : List (Html.Attribute msg) -> List (Html msg) -> Html msg
 withHeader attrs content =
-    node contentNode attrs ((div [] [ h1 [] [ text " D'LayDOS" ] ]) :: content)
+    node contentNode attrs <|
+        flip (::) content <|
+            div [] [ h1 [] [ text " D'LayDOS" ] ]
