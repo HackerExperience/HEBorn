efd246f12f23208d2d3da93001cae8d9ae82cf17
themaxhero <marcelo.a.lima.santos@gmail.com>
2018-01-11 21:33:26 -0200

    Implement FloatingHeads App

--- src/Apps/Email/Messages.elm
+++ src/Apps/Email/Messages.elm
@@ -1,10 +1,10 @@
 module Apps.Email.Messages exposing (Msg(..))
 
 import Apps.Email.Menu.Messages as Menu
+import Game.Storyline.Emails.Models as Emails exposing (Person)
 import Game.Storyline.Emails.Contents exposing (Content)
 
 
 type Msg
     = MenuMsg Menu.Msg
     | SelectContact String
-    | Reply Content
--- src/Apps/Email/View.elm
+++ src/Apps/Email/View.elm
@@ -3,8 +3,13 @@ module Apps.Email.View exposing (view)
 import Dict
 import Html exposing (..)
 import Html.CssHelpers
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Css
+import Css.Utils exposing (styles)
 import Game.Data as Game
+import Game.Models as Game
+import Game.Storyline.Models as Storyline
 import Game.Storyline.Emails.Models as Emails exposing (ID, Person)
 import Game.Storyline.Emails.Contents as Emails
 import Apps.Email.Messages exposing (Msg(..))
@@ -21,105 +26,59 @@ view : Game.Data -> Model -> Html Msg
 view data model =
     let
         emails =
-            data.game.story.emails
+            data
+                |> Game.getGame
+                |> Game.getStory
+                |> Storyline.getEmails
 
         contactList =
             emails
-                |> Dict.foldr (contactProcessor model.activeContact) []
+                |> Dict.foldr contact []
                 |> ul [ class [ Contacts ] ]
-
-        active =
-            model.activeContact
-                |> Maybe.andThen (\k -> Dict.get k emails)
     in
         div
-            [ menuForDummy
-            , class [ Super ]
-            ]
+            [ menuForDummy ]
             [ contactList
-            , mainChat active
             , menuView model
             ]
 
 
-mainChat : Maybe Person -> Html Msg
-mainChat active =
-    active
-        |> Maybe.map Emails.getAvailableReplies
-        |> Maybe.withDefault []
-        |> List.map reply
-        |> div []
-        |> List.singleton
-        |> (::) (ul [] (mainChatMessages active))
-        |> div [ class [ MainChat ] ]
-
-
-reply : Emails.Content -> Html Msg
-reply msg =
-    Emails.toString msg
-        |> text
-        |> List.singleton
-        |> span [ onClick <| Reply msg ]
-
-
-mainChatMessages : Maybe Person -> List (Html Msg)
-mainChatMessages active =
-    active
-        |> Maybe.map (Emails.getMessages >> Dict.values)
-        |> Maybe.withDefault []
-        |> List.map
-            (\v ->
-                case v of
-                    Emails.Sent msg ->
-                        baloon To msg
-
-                    Emails.Received msg ->
-                        baloon From msg
-            )
-
-
-baloon : Classes -> Emails.Content -> Html Msg
-baloon direction msg =
-    li
-        [ class [ direction ] ]
-        [ content msg ]
-
-
-content : Emails.Content -> Html Msg
-content msg =
-    Emails.toString msg
-        |> text
-        |> List.singleton
-        |> span []
-
-
-contactProcessor :
-    Maybe ID
-    -> ID
+contact :
+    ID
     -> Person
     -> List (Html Msg)
     -> List (Html Msg)
-contactProcessor activeKey k { about } acu =
+contact k { about } acu =
     let
-        activeAttr =
-            case activeKey of
-                Just activeKey ->
-                    (if k == activeKey then
-                        [ class [ Active ] ]
-                     else
-                        []
-                    )
+        name =
+            about
+                |> Maybe.map (.name)
+                |> Maybe.withDefault "[UNKNOWN]"
 
-                _ ->
-                    []
+        source =
+            case about of
+                Just about_ ->
+                    [ src about_.picture ]
+
+                Nothing ->
+                    [ src "https://pbs.twimg.com/profile_images/928805578679431168/zwSXRn0K_400x400.jpg" ]
+
+        image =
+            img imageAttrs []
+
+        imageAttrs =
+            [ class [ Avatar ] ] ++ imageSize ++ source
+
+        imageSize =
+            [ Css.width (Css.px 48)
+            , Css.height (Css.px 48)
+            , Css.borderRadius (Css.pct 100)
+            ]
+                |> styles
+                |> List.singleton
 
         attrs =
-            (onClick <| SelectContact k) :: activeAttr
+            [ onClick <| SelectContact k ]
     in
-        about
-            |> Maybe.map (.name)
-            |> Maybe.withDefault "[UNKNOWN]"
-            |> text
-            |> List.singleton
-            |> li attrs
+        li attrs [ image, text name ]
             |> flip (::) acu
