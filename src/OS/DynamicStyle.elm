2f468fbd7866ed1f9b38dc077c3d2ba691e31e75
PedroHLC <root@pedrohlc.com>
2018-01-15 18:54:29 -0200

    Better Dynamic Styles

--- src/OS/DynamicStyle.elm
+++ src/OS/DynamicStyle.elm
@@ -1,23 +1,48 @@
 module OS.DynamicStyle exposing (..)
 
-import Html exposing (Html, node)
-import Html.Attributes exposing (property)
+import Html exposing (Html, node, text)
+import Html.Attributes exposing (property, id)
+import Html.Lazy exposing (lazy)
 import Json.Encode as Json
+import Css exposing (Stylesheet)
 import Css.File
-import OS.Messages exposing (Msg)
-import Game.Storyline.Missions.Models as Missions
+import Game.Models as Game
+import Game.Storyline.DynamicStyle as Storyline
 import Game.Storyline.Missions.DynamicStyle as Missions
+import OS.Messages exposing (Msg)
+import UI.DynStyles.SimplePlan.Apps exposing (..)
 
 
-view : Missions.Model -> Html Msg
-view missions =
-    missions
-        |> Missions.dynCss
-        |> List.singleton
-        |> Css.File.compile
-        |> (.css)
-        |> Json.string
-        |> property "innerHTML"
-        |> List.singleton
-        |> node "style"
-        |> (\z -> z [])
+styleNode : String -> List Stylesheet -> Html Msg
+styleNode id_ stylesheet =
+    node "style"
+        [ id (id_ ++ "DynStyle")
+        , stylesheet
+            |> Css.File.compile
+            |> (.css)
+            |> Json.string
+            |> property "innerHTML"
+        ]
+        []
+
+
+view : Game.Model -> List (Html Msg)
+view { story } =
+    let
+        missions_ =
+            Missions.dynCss
+                >> styleNode "missions"
+
+        story_ =
+            Storyline.dynCss
+                >> styleNode "storyline"
+
+        ( storyStyles, missionsStyles ) =
+            if story.enabled then
+                ( lazy story_ story
+                , lazy missions_ story.missions
+                )
+            else
+                ( text "", text "" )
+    in
+        [ storyStyles ]
