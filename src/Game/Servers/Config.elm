9cbc5dd16e4126f8d3378559bd3e7313c25bf21b
themaxhero <marcelo.a.lima.santos@gmail.com>
2018-01-17 23:47:51 -0200

    Refactor Game.Servers.Logs.Update to use Configs

--- src/Game/Servers/Config.elm
+++ src/Game/Servers/Config.elm
@@ -1,8 +1,9 @@
-module Game.Servers.Config exposing (Config, processesConfig)
+module Game.Servers.Config exposing (..)
 
 import Time exposing (Time)
 import Core.Flags as Core
 import Game.Meta.Types.Network as Network exposing (NIP)
+import Game.Servers.Logs.Config as Logs
 import Game.Servers.Processes.Config as Processes
 import Game.Servers.Messages exposing (..)
 import Game.Servers.Shared exposing (..)
@@ -22,4 +23,12 @@ processesConfig cid nip config =
     , cid = cid
     , nip = nip
     , lastTick = config.lastTick
+    }
+
+
+logsConfig : CId -> Config msg -> Logs.Config msg
+logsConfig cid config =
+    { flags = config.flags
+    , toMsg = LogsMsg >> ServerMsg cid >> config.toMsg
+    , cid = cid
     }
