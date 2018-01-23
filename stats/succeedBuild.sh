3aaf59b7a114b7b2e079d7cbf9223a1c240c800e
PedroHLC <root@pedrohlc.com>
2018-01-23 12:38:23 -0200

    Change setup next button behaviour

--- stats/succeedBuild.sh
+++ stats/succeedBuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd 'stats'
 read -r num < failedAttempts.dat
-if [[ "$num" -gt 2 ]]; then
+if [[ "$num" -gt 6 ]]; then
     if [ -x "$(command -v mpv)" ]; then
         mpv https://archive.org/download/TemaDaVitriaAyrtonSenna/TemaDaVitoria-AyrtonSenna.mp3 &
     fi
