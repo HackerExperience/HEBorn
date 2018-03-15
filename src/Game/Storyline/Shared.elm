4edce08bf42623f2f640df937b4b15d006f56233
PedroHLC <root@pedrohlc.com>
2018-03-14 22:55:55 -0300

    Expand Storyline to include NastyVirus sub-chapter

--- src/Game/Storyline/Shared.elm
+++ src/Game/Storyline/Shared.elm
@@ -20,11 +20,16 @@ type PastEmail
 type Reply
     = AboutThat
     | BackThanks
+    | DlaydMuch1
     | DownloadCracker1 SomeIP
     | Downloaded
     | HellYeah
     | NastyVirus1
+    | NastyVirus2
     | NothingNow
+    | Punks1
+    | Punks2
+    | Punks3 SomeIP
     | WatchIADoing
     | Welcome
     | YeahRight
